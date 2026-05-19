0b2e68073e7a0a64436e108cc4f2cd986b581d44
tirth65 <tirthsheth83@gmail.com>
2026-05-19 12:10:19 +0530

    Update homepage version 2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send(`
-    <h1>Version 1: Hello from GitHub Actions 🚀</h1>
+    <h1>Version 2: CI/CD is working ✅</h1>
     <p>This Node.js app is deployed on AWS EC2 without Docker.</p>
   `);
 });
